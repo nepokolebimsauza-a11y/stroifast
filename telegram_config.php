5b7ebfabd9a10038c9ee882dcf90fa1ae5466fad
nepokolebimsauza-a11y <nepokolebimsauza@gmail.com>
2026-04-12 22:46:56 +0300

    Add files via upload
    Signed-off-by: nepokolebimsauza-a11y <nepokolebimsauza@gmail.com>

---
+++ telegram_config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Локальная конфигурация (не коммитится). Токен выдать заново в @BotFather при утечке.
+ */
+return [
+    'bot_token' => '8744713942:AAEMUdbvjWf94zPEQyTHbSHRkVr5ppvm6j4',
+    'chat_id' => '8744713942',
+];
